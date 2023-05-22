e945b81a128b073a27cf5c5db07961a46605d4df
zhaowj2023 <134300966+zhaowj2023@users.noreply.github.com>
2023-05-22 23:15:29 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -20,13 +20,13 @@ const TEMPLATE_CONFIG = [
       
       ---
       
-      今天是我们相识的第{{love_day.DATA}}天
-      
-      {{birthday_message.DATA}}
+     {{earthy_love_words.DATA}}
       
       ---
+     
+      今天是我与老婆相识的第{{love_day.DATA}}天
       
-      {{earthy_love_words.DATA}}
+      {{birthday_message.DATA}}
       
       ---
     `
